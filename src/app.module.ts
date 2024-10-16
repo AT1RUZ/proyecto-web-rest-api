22057d473dcd71b0454b86aa19736c859147f299
AT1RUZ <dtzurita2003@gmail.com>
2024-10-16 18:14:41 -0400

    Primer Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TransporteModule } from './transporte/transporte.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'asd',
+      database: 'GestionRefugio',
+      entities: [],
+      logging: true,
+    }),
+    UserModule,
+    TransporteModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
