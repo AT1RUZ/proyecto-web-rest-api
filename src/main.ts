bb634e842a4521f9554ad16be6841d397e367ae1
AT1RUZ <dtzurita2003@gmail.com>
2024-10-17 03:22:52 -0400

    Añadiendo uso de CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001', // O usa '*' para permitir todos los orígenes
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
