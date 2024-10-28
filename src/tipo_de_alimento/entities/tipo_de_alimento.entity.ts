a514819fc35c0f5497aac55d832f4b332529f210
AT1RUZ <dtzurita2003@gmail.com>
2024-10-28 04:53:58 -0400

    Arregladiña

--- src/tipo_de_alimento/entities/tipo_de_alimento.entity.ts
+++ src/tipo_de_alimento/entities/tipo_de_alimento.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryColumn } from "typeorm";
 
-@Entity("Tipo_de_Alimento")//aki se tiene que poner igual que en la tabla de la base de datos
+@Entity()
 export class TipoDeAlimento {
     @PrimaryColumn({type: 'varchar'})
     ID_Alimento: string;
--- src/transporte/entities/transporte.entity.ts
+++ src/transporte/entities/transporte.entity.ts
@@ -1,6 +1,6 @@
 import { Entity, Column, PrimaryColumn } from 'typeorm';
 
-@Entity("Transporte")//aki se tiene que poner igual que en la tabla de la base de datos
+@Entity()
 export class Transporte {
   @PrimaryColumn({ type: 'varchar' })
   ID_Transporte: string;
