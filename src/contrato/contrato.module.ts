5faae3c1c4a2192bcf520adf33616e0b5f0b7c44
Diego Torres <dtzurita2003@gmail.com>
2024-11-13 03:15:35 -0500

    asd

--- src/contrato/contrato.module.ts
+++ src/contrato/contrato.module.ts
@@ -8,6 +8,6 @@ import { Contrato } from './entities/contrato.entity';
   imports: [TypeOrmModule.forFeature([Contrato])],
   controllers: [ContratosController],
   providers: [ContratosService],
-  exports: [ContratosService]
+  exports: [ContratosService],
 })
 export class ContratosModule {}
